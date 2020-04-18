c81b5699555f6400a81b41002567916d85a47ad7
Asheesh Laroia <asheesh@asheesh.org>
2020-04-17 19:42:09 -0700

    wip

---
+++ src/helloworld/__main__.py
@@ -0,0 +1,4 @@
+from helloworld.app import main
+
+if True or __name__ == '__main__':
+    main().main_loop()
---
+++ src/helloworld/app.py
@@ -0,0 +1,33 @@
+"""
+My first application
+"""
+import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
+
+
+class HelloWorld(toga.App):
+
+    def startup(self):
+        """
+        Construct and show the Toga application.
+
+        Usually, you would add your application to a main content box.
+        We then create a main window (with a name matching the app), and
+        show the main window.
+        """
+        main_box = toga.Box()
+
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+        self.main_window.show()
+
+
+def main():
+    # XXX Hack
+    class HasDunderPackage:
+        __package__ = 'helloworld'
+    import sys
+    sys.modules['__main__'] = HasDunderPackage
+    ## XXX End hack
+    return HelloWorld()
